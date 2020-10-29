c3f810d42d121ef77936515293c2f706a1897f24
Craig McFarland <craigtmcfarland@gmail.com>
2020-10-29 11:22:41 -0400

    finished ex 2

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -22,42 +22,37 @@ namespace IterationStatements
             } while (myNumber<100);
 
 
-
-
-         
-            while(myNumber < 200)
+            while (myNumber < 200)
 
             {
-              
+
                 myNumber++;
 
-                    numbers.Add(myNumber);
-            }
+                numbers.Add(myNumber);
 
+
+            }
             Console.WriteLine("increase");
 
-               
 
-            foreach(var item in numbers)
+            foreach (var item in numbers)
             {
                 Console.WriteLine(item);
             }
 
 
-
-        
-
-
-
-            // Create a for loop
-            // in your initializer set the value of i to 199
-            // in your conditional, as long as i is less than or equal to the amount of items in "numbers" (numbers.Count)
-            // and as long as i is greater than or equal to 0
-            // Decrement i by 1
-
-            for(int i = 199;i <= numbers.Count && i >= 0; i--)
+            Console.WriteLine("decrease");
+            for (int i = 199; i <= numbers.Count && i >= 0; i--)
             {
                 Console.WriteLine($"{numbers[i]}");
+
+
+
+
+
+
+
+
 
             }
         }
